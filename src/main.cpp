cc27caea963116fa17cda738ac0e8f88debe5b25
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2024-09-27 14:52:56 +0100

    completed!

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,45 @@
 
-#include <bitset>
+#include "src/Chip8.hpp"
+#include "src/platform.hpp"
 #include <iostream>
 
-int main(int argc, char *argv[]) {
-  // SDL_Window *window;
-  // SDL_Renderer *renderer;
-  // SDL_Event event;
-  //
-  // if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-  //   SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s",
-  //                SDL_GetError());
-  //   return 3;
-  // }
-  //
-  // if (SDL_CreateWindowAndRenderer(320, 240, SDL_WINDOW_RESIZABLE, &window,
-  //                                 &renderer)) {
-  //   SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-  //                "Couldn't create window and renderer: %s", SDL_GetError());
-  //   return 3;
-  // }
-  //
-  // while (1) {
-  //   SDL_PollEvent(&event);
-  //   if (event.type == SDL_QUIT) {
-  //     break;
-  //   }
-  //   SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
-  //   SDL_RenderClear(renderer);
-  //   SDL_RenderPresent(renderer);
-  // }
-  //
-  // SDL_DestroyRenderer(renderer);
-  // SDL_DestroyWindow(window);
-  //
-  // SDL_Quit();
-  //
-  uint8_t registers[16]{};
+int main(int argc, char **argv) {
+  if (argc != 4) {
+    std::cerr << "Usage: " << argv[0] << " <Scale> <Delay> <ROM>\n";
+    std::exit(EXIT_FAILURE);
+  }
 
-  registers[0] = 5;   // Store the value 5 in the first register
-  registers[1] = 10;  // Store the value 10 in the second register
-  registers[2] = 255; // Store the value 255 in the third register (maximum
+  int videoScale = std::stoi(argv[1]);
+  int cycleDelay = std::stoi(argv[2]);
+  char const *romFilename = argv[3];
 
-  // Example of accessing the values
-  std::cout << "register 0: " << static_cast<int>(registers[0]) << std::endl;
-  std::cout << "register 1: " << static_cast<int>(registers[1]) << std::endl;
-  std::cout << "register 2: " << static_cast<int>(registers[2]) << std::endl;
+  Platform platform("CHIP-8 Emulator", VIDEO_WIDTH * videoScale,
+                    VIDEO_HEIGHT * videoScale, VIDEO_WIDTH, VIDEO_HEIGHT);
 
-  printf("\n");
+  Chip8 chip8;
+  chip8.LoadROM(romFilename);
 
-  std::cout << "register 0: " << std::bitset<8>(registers[0]) << std::endl;
-  std::cout << "register 1: " << std::bitset<8>(registers[1]) << std::endl;
-  std::cout << "register 2: " << std::bitset<8>(registers[2]) << std::endl;
+  int videoPitch = sizeof(chip8.video[0]) * VIDEO_WIDTH;
+
+  auto lastCycleTime = std::chrono::high_resolution_clock::now();
+  bool quit = false;
+
+  while (!quit) {
+    quit = platform.ProcessInput(chip8.keypad);
+
+    auto currentTime = std::chrono::high_resolution_clock::now();
+    float dt = std::chrono::duration<float, std::chrono::milliseconds::period>(
+                   currentTime - lastCycleTime)
+                   .count();
+
+    if (dt > cycleDelay) {
+      lastCycleTime = currentTime;
+
+      chip8.Cycle();
+
+      platform.Update(chip8.video, videoPitch);
+    }
+  }
 
   return 0;
 }
